87167189fde1441fb9bbc7da7b35ee715fff257e
0ya-sh0 <ymthatte@gmail.com>
2019-05-28 19:06:46 +0530

    restructre

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+app.use('/public',express.static('public'));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
+  response.redirect('/index')
+});
+
+app.get('/index', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
